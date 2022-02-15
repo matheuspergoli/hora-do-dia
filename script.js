38ca00218fdf0e88b5af66a73c5de2d0124d3490
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-02-15 00:41:04 -0300

    update

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-let agora = new Date()
-let horario = agora.getHours()
-let imagem = window.document.getElementById('container-imagem')
-let fundo = window.document.querySelector('body')
+var hora = new Date()
+var horario = hora.getHours()
 
-let hora = window.document.querySelector('#container > p')
+var text = window.document.getElementById('text')
+var image = window.document.getElementById('image')
+var bgColor = window.document.getElementById('background')
 
-if (horario <= 5) {
-    hora.innerText = `Agora são ${horario} horas da madrugada`
-    imagem.style.backgroundImage = 'url(imagens/madrugada.jpg)'
-    imagem.style.backgroundPositionX = 'right'
-    imagem.style.backgroundSize = 'cover'
-    fundo.style.backgroundColor = '#0a3d62'
-} else if (horario <= 11) {
-    hora.innerText = `Agora são ${horario} horas da manhã`
-    imagem.style.backgroundImage = 'url(imagens/manha.jpg)'
-    imagem.style.backgroundSize = 'cover'
-    fundo.style.backgroundColor = '#fdcb6e'
-} else if (horario <= 18) {
-    hora.innerText = `Agora são ${horario} horas da tarde`
-    imagem.style.backgroundImage = 'url(imagens/tarde.jpg)'
-    imagem.style.backgroundPositionX = 'right'
-    imagem.style.backgroundSize = 'cover'
-    fundo.style.backgroundColor = '#f8c291'
+if (horario >= 1 && horario <= 5) {
+    text.innerHTML = `Agora são <strong>${horario} horas</strong> da madrugada`
+    image.style.backgroundImage = 'url(imagens/madrugada.jpg)'
+    bgColor.style.backgroundColor = '#0A3D62'
+} else if (horario >= 6 && horario <= 11) {
+    text.innerHTML = `Agora são <strong>${horario} horas</strong> da manhã`
+    image.style.backgroundImage = 'url(imagens/manha.jpg)'
+    bgColor.style.backgroundColor = '#FDCB6E'
+} else if (horario >= 12 && horario <= 16) {
+    text.innerHTML = `Agora são <strong>${horario} horas</strong> da tarde`
+    image.style.backgroundImage = 'url(imagens/tarde.jpg)'
+    bgColor.style.backgroundColor = '#F8C291'
 } else {
-    hora.innerText = `Agora são ${horario} horas da noite`
-    imagem.style.backgroundImage = 'url(imagens/noite.jpg)'
-    imagem.style.backgroundSize = 'cover'
-    fundo.style.backgroundColor = '#57606f'
-} 
+    text.innerHTML = `Agora são <strong>${horario} horas</strong> da noite`
+    image.style.backgroundImage = 'url(imagens/noite.jpg)'
+    bgColor.style.backgroundColor = '#0A3D62'
+}
